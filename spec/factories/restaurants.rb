2802e4d6868d42ffa1463f217f8590155ba3701c
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-07-21 12:02:34 -0700

    add get request specs and pass them

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :restaurant do
-
+    name(Faker::Company.name)
+    address(Faker::Address.street_address)
+    phone_number(Faker::Number.number(10))
   end
+
 end
---
+++ spec/requests/get_restaurants_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "get all restaurant route", :type => :request do
+  let!(:restaurants) { FactoryGirl.create_list(:restaurant, 20)}
+
+  before { get '/restaurants'}
+
+  it 'returns all restaurants' do
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
